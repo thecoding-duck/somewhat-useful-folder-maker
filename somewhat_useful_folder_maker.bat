f86accbb9213bccfccc71404558e907056b7073d
thecoding-duck <cyborgcreations01@gmail.com>
2026-08-19 13:51:36 -0700

    Fix folder creation path in batch script

--- somewhat_useful_folder_maker.bat
+++ somewhat_useful_folder_maker.bat
@@ -3,7 +3,7 @@ setlocal
 title somewhat_useful
 echo This tool only creates folders.. do u wanna create one?
 pause
-mkdir C:\Users\User\Downloads\somewhat_useful_folder
+mkdir "C:\Users\User\Downloads"
 echo there you go! theres nothing in the folder.. its called, "somewhat_useful_folder"
 
 echo you dont need to wait 20 secs, it just closes out after that long
